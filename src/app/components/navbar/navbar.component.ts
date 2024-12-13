1f1addff99ee8633fe24c6a80cc5145366a73b7a
raj-pwr <pawar@partikel-art.de>
2024-12-13 15:26:25 +0100

    Make navbar responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  public isMenuOpen: Boolean = false;
+  public innerWidth: number = window.innerWidth;
 
+  @HostListener('window:resize', ['$event'])
+  public onResize(event: Event): void {
+    this.innerWidth = (event.target as Window).innerWidth;
+    if (this.innerWidth >= 768 && this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
+  }
+
+  public closeMenu(): void {
+    this.isMenuOpen = false;
+  }
 }

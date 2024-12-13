fc8b170c5e7b25371c1ed35675e816398ead99e3
raj-pwr <pawar@partikel-art.de>
2024-12-13 22:47:40 +0100

    Complete the contact send email component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import emailjs from 'emailjs-com';
+import { environment } from '../../../environments/environment.prod';
 
 @Component({
   selector: 'app-contact',
@@ -7,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  public sendEmail(event: Event): void {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    
+    emailjs.sendForm(environment.emailServiceId, environment.emailTemplateId, form, environment.emailPublicKey)
+      .then((result) => {
+        console.log('Email successfully sent!', result.text);
+        alert('Your message has been sent successfully!');
+        form.reset();
+      }, (error) => {
+        console.error('Error sending email:', error.text);
+        alert('Oops! Something went wrong. You can contact me at er.rajpawar@gmail.com');
+      });
+  }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    emailServiceId: 'REPLACE_EMAILJS_SERVICE_ID',
+    emailTemplateId: 'REPLACE_EMAILJS_TEMPLATE_ID',
+    emailPublicKey: 'REPLACE_EMAILJS_PUBLIC_KEY'
+};

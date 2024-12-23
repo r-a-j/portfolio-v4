fb1d4d3565920d0a201568d07ab8d9eb5e4b85bc
r-a-j <er.rajpawar@gmail.com>
2024-12-24 00:53:09 +0100

    Add all components on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { ExperienceComponent } from "../experience/experience.component";
+import { ProjectsComponent } from "../projects/projects.component";
+import { SkillsComponent } from "../skills/skills.component";
+import { ContactComponent } from "../contact/contact.component";
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent],
+  imports: [
+    HeroComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

5f2d86a9a11c09fb1142f2814e322601a497be54
r-a-j <er.rajpawar@gmail.com>
2025-06-09 11:19:01 +0200

    Enhance router configuration with in-memory scrolling options

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
@@ -8,7 +8,13 @@ import { MarkdownModule, MARKED_OPTIONS } from 'ngx-markdown';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled'
+      })
+    ),
     provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
     ...(MarkdownModule.forRoot({}).providers || []),
